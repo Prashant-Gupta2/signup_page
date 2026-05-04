7591a32ab063b3d272831bd51e67ab2e3f1c6369
Prashant-Gupta2 <161327022+Prashant-Gupta2@users.noreply.github.com>
2026-05-04 21:55:53 +0530

    update

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,19 +1,28 @@
 require('dotenv').config();
-const {Sequelize} = require('sequelize');
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+const { Sequelize } = require('sequelize');
+
 const sequelize = new Sequelize(
-  'mydb',
-  'root',
+  process.env.DB_NAME,
+  process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
-    dialect: 'mysql'
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false
   }
 );
+
+
 sequelize.authenticate()
-.then(()=>{
- console.log("Database connected")
-}).catch((err)=>{
- console.log(err)
-})
+  .then(() => {
+    console.log(" Database connected");
+  })
+  .catch((err) => {
+    console.log(" DB connection error:", err);
+  });
 
 module.exports = sequelize;
